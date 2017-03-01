b2b1b30b83852db2330977ca773331abc8b0be88
Brandon Ford <bford@wireless-10-147-43-37.public.utexas.edu>
2017-03-01 13:22:52 -0600

    Initial Commit

---
+++ Login PageTests/Login_PageTests.m
@@ -0,0 +1,39 @@
+//
+//  Login_PageTests.m
+//  Login PageTests
+//
+//  Created by Brandon Ford on 3/1/17.
+//  Copyright © 2017 dynaConnections. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Login_PageTests : XCTestCase
+
+@end
+
+@implementation Login_PageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
